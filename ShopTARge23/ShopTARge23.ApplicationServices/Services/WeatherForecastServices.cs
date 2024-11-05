ce59c9527689524000e4653f92ddb499379b0a69
Ingvar Derešivski <inde999@gmail.com>
2024-11-05 13:35:09 +0200

    AccuWeatherInProgress

---
+++ ShopTARge23/ShopTARge23.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,25 @@
+﻿using Nancy.Json;
+using ShopTARge23.Core.Dto.WeatherDtos.AccuWeatherDtos;
+using System.Net;
+
+
+namespace ShopTARge23.ApplicationServices.Services
+{
+    public class WeatherForecastServices
+    {
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        {
+            string accuApiKey = "2LBA498kppCQjyb9ZAh5IgNuMYgZZDEr";
+            string url = $"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={accuApiKey}&q={dto.CityName}";
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                AccuLocationRootDto accuResult = new JavaScriptSerializer().Deserialize<AccuLocationRootDto>(json);
+            }
+
+            return dto;
+        }
+    }
+}
---
+++ ShopTARge23/ShopTARge23.Core/Dto/WeatherDtos/AccuWeatherDtos/AccuLocationWeatherResultDto.cs
@@ -0,0 +1,46 @@
+﻿namespace ShopTARge23.Core.Dto.WeatherDtos.AccuWeatherDtos
+{
+    public class AccuLocationWeatherResultDto
+    {
+        public string CityName { get; set; }
+        public string CityCode { get; set; }
+        public int Rank { get; set; }
+
+
+        public string EffectiveDate { get; set; }
+        public Int64 EffectiveEpochDate { get; set; }
+        public int Severity { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+        public string EndDate { get; set; }
+        public Int64 EndEpochDate { get; set; }
+
+
+        public string DailyForecastsDate { get; set; }
+        public int DailyForecastsEpochDate { get; set; }
+
+        public double TempMinValue { get; set; }
+        public string TempMinUnit { get; set; }
+        public int TempMinUnitType { get; set; }
+
+        public double TempMaxValue { get; set; }
+        public string TempMaxUnit { get; set; }
+        public int TempMaxUnitType { get; set; }
+
+        public int DayIcon { get; set; }
+        public string DayIconPhrase { get; set; }
+        public bool DayHasPrecipitation { get; set; }
+        public string DayPrecipitationType { get; set; }
+        public string DayPrecipitationIntensity { get; set; }
+
+        public int NightIcon { get; set; }
+        public string NightIconPhrase { get; set; }
+        public bool NightHasPrecipitation { get; set; }
+        public string NightPrecipitationType { get; set; }
+        public string NightPrecipitationIntensity { get; set; }
+
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+        //iseseisvalt kirjutate siia muutujad
+    }
+}
