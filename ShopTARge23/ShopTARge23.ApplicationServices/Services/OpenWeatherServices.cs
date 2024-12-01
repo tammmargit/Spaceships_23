09350e24ca1ac498b56f78a3613b53b52948b8c6
margittammeorg <margit.tammeorg@gmail.com>
2024-12-01 18:27:29 +0200

    Services

--- ShopTARge23/ShopTARge23.ApplicationServices/Services/OpenWeatherServices.cs
+++ ShopTARge23/ShopTARge23.ApplicationServices/Services/OpenWeatherServices.cs
@@ -1,11 +1,42 @@
-﻿namespace ShopTARge23.ApplicationServices.Services
+﻿using Nancy.Json;
+using ShopTARge23.Core.Dto.WeatherDtos.OpenWeatherDtos;
+using ShopTARge23.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARge23.ApplicationServices.Services
 {
     public class OpenWeatherServices
     {
-        // api key = 3838f6d77378b904e2784adbb7811565
-        // https://api.openweathermap.org/data/2.5/weather?q={CityName}&appid={ApiKey}&units=metric
-        // https://api.openweathermap.org/data/2.5/onecall?lat={Latitude}&lon={Longitude}&exclude=hourly,minutely,alerts&appid={ApiKey}&units=metric
-        // https://api.openweathermap.org/data/2.5/onecall?lat=51.507351&lon=-0.127758&exclude=hourly,minutely,alerts&appid=3838f6d77378b904e2784adbb7811565&units=metric
-        // https://api.openweathermap.org/data/2.5/onecall?lat=51.507351&lon=-0.127758&exclude=hourly,minutely,alerts&appid=3838f6d77378b904e2784adbb7811565&units=metric
+        //api key = 3838f6d77378b904e2784adbb7811565
+        //https://api.openweathermap.org/data/2.5/weather?q={CityName}&appid={ApiKey}&units=metric
+        public async Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto)
+        {
+            string openApiKey = "3838f6d77378b904e2784adbb7811565";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City}&units=metric&appid={openApiKey}";
+
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                OpenWeatherRootDto weatherRootDto = new JavaScriptSerializer().Deserialize<OpenWeatherRootDto>(json);
+                dto.City = weatherRootDto.Name;
+                dto.Temp = weatherRootDto.Main.Temp;
+                dto.FeelsLike = weatherRootDto.Main.FeelsLike;
+                dto.Humidity = weatherRootDto.Main.Humidity;
+                dto.Pressure = weatherRootDto.Main.Pressure;
+                dto.WindSpeed = weatherRootDto.Wind.Speed;
+                dto.Description = weatherRootDto.Weather[0].Description;
+
+            }
+
+            return dto;
+
+        }
     }
 }
